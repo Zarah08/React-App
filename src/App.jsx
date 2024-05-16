49de53ca06b1a8e2881c0f0868bcd720e84a4278
zarah <zarahabubakar1234@gmail.com>
2024-05-16 14:46:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 style={{textAlign:'center',color:'dodgerblue'}}>Welcome to React App Tutorial</h1>
+
+    </>
+  )
+}
+
+export default App
